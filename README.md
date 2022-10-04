cc5ca5d476e8c6089f1d1ddb397b9bad1d195cd3
lucasribolli <lucasribolli@gmail.com>
2022-10-04 08:41:15 -0300

    fix post route and add medicine get route

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-## Main endpoint
-> https://master--papaya-bonbon-dfcc00.netlify.app/
-
 ## The endpoints are accessed by:
-> https://master--papaya-bonbon-dfcc00.netlify.app/.netlify/functions/api/**EXPRESS_ENDPOINT**
+> https://master--papaya-bonbon-dfcc00.netlify.app/.netlify/functions/api/**ENDPOINT**
+
+## Development tips
+
+Create a new branch
+
+Install local dependencies:
+> npm install
+
+Run the server:
+> npm start
+
+Test your endpoints:
+> http://localhost:9000/.netlify/functions/api/**ENDPOINT**
+
+If everything goes fine, merge your branch to master
--- src/api.js
+++ src/api.js
@@ -8,21 +8,20 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient } = require('mongodb');
-const HOSPITOQUE_DB_NAME = "hospitoque_db";
+const HOSPITOQUE_DB_NAME = 'hospitoque_db';
 
 router.get('/', (req, res) => {
   res.json({
-    'hello': 'hi!'
+    'hospitoque': 'hospitoque'
   });
 });
 
-const COLLECTION_MEDICINE = "medicines";
+const COLLECTION_MEDICINE = 'medicines';
 
-// This section will help you get a list of all the records.
-router.route('/add-medicine').post(async function (_req, res) {
+router.route('/medicine').post(async function (req, res) {
   const medicine = {
     name: req.body.name,
-    manufacturer: manufacturer,
+    manufacturer: req.body.manufacturer,
     composition: req.body.composition,
     variant: req.body.variant,
     creationDate: new Date()
@@ -36,10 +35,28 @@ router.route('/add-medicine').post(async function (_req, res) {
         res.status(400).send('Error inserting medicine!');
       } else {
         console.log(`Added a new medicine with id ${result.insertedId}`);
-        res.status(204).send();
+        res.send();
       }
     });
   });
+  client.close();
+});
+
+// This section will help you get a list of all the records.
+router.route('/medicine').get(async function (_, res) {
+  const client = getClient();
+  client.connect(async function (_) {
+    const collection = client.db(HOSPITOQUE_DB_NAME).collection(COLLECTION_MEDICINE);
+    collection
+      .find({})
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).send('Error fetching medicine!');
+        } else {
+          res.json(result);
+        }
+      });
+    })
   client.close();
 });
 
