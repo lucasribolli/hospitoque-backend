3a42a387174d74c66e1f3241a6548742f55e6da3
lucasribolli <lucasribolli@gmail.com>
2022-10-05 19:34:49 -0300

    add auth route

--- src/api.js
+++ src/api.js
@@ -17,6 +17,7 @@ router.get('/', (req, res) => {
 });
 
 const COLLECTION_MEDICINE = 'medicines';
+const COLLECTION_USERS = 'users';
 
 router.route('/medicine').post(async function (req, res) {
   const medicine = {
@@ -42,7 +43,6 @@ router.route('/medicine').post(async function (req, res) {
   client.close();
 });
 
-// This section will help you get a list of all the records.
 router.route('/medicine').get(async function (_, res) {
   const client = getClient();
   client.connect(async function (_) {
@@ -54,6 +54,28 @@ router.route('/medicine').get(async function (_, res) {
           res.status(400).send('Error fetching medicine!');
         } else {
           res.json(result);
+        }
+      });
+    })
+  client.close();
+});
+
+router.route('/auth').get(async function (req, res) {
+  var email = req.query.email
+  const client = getClient();
+  client.connect(async function (_) {
+    const collection = client.db(HOSPITOQUE_DB_NAME).collection(COLLECTION_USERS);
+    collection
+      .find({
+        email: email
+      })
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).send('Error on auth ' + err);
+        } else {
+          res.json({
+            authorized: result.length > 0
+          });
         }
       });
     })
